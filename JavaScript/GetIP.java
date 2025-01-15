ac30464298fea3a3da1d17d141683877ac86d7fa
asitshakya678 <asitshakya789@gmail.com>
2025-01-15 23:25:46 +0530

    Refactor GetIP class to remove commented code and improve readability

--- JavaScript/GetIP.java
+++ JavaScript/GetIP.java
@@ -4,13 +4,10 @@ import java.net.UnknownHostException;
 public class GetIP {
     public static void main(String[] args) {
         try {
-            // Get the InetAddress object representing the local host
             InetAddress localHost = InetAddress.getLocalHost();
             
-            // Get the IP address as a string
             String ipAddress = localHost.getHostAddress();
             
-            // Display the IP address
             System.out.println("Your device's IP address is: " + ipAddress);
         } catch (UnknownHostException e) {
             System.out.println("Unable to get IP address");
@@ -18,3 +15,4 @@ public class GetIP {
         }
     }
 }
+        
